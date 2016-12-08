909ed8454f45f9a534adc7f6e83824132bbc322a
Bosch, Melanie van den <melanie.vandenbosch@wur.nl>
2016-12-08 09:52:32 +0100

    added some comments for better readability and writing the genes of interest to a file

--- R_script.R
+++ R_script.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env R
-#setwd("//SCOMP0856/veen119$/My Documents/Minor 2016/BIF 30806 Advanced Bioinformatics/Project")
+#setwd("//scomp0854/bosch098$/My Documents/Adv. Bioinformatics/project")
 args=(commandArgs(TRUE))
 
 if(length(args)==0){
@@ -7,7 +7,7 @@ if(length(args)==0){
   ##supply default values
   infile = "http://www.bioinformatics.nl/courses/BIF-30806/maize_e3.table"
   g.o.i = "GRMZM2G054123"
-  method = "complete"
+  method = "complete" # can also change method to average.
   parameters = c(6,2000)
 }else{
   for(i in 1:length(args)){
@@ -15,18 +15,20 @@ if(length(args)==0){
   }
 }
 
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("DESeq2")
-#library(DESeq2)
-#source("http://www.bioinformatics.nl/courses/BIF-30806/DEseq2Exercise.R")
 
 test_data=read.table(
   infile, 
   row.names=1, header=TRUE, sep ="\t")
 
+
+#takes 2000 samples
 subnumber <- parameters[2]
+# removes last column and normalizes the samples on sd=1
 sds <- apply(test_data[,-7], 1, sd)
+# orders the samples and returns indices of first the sample with higest sd
 ind <- order(sds,decreasing=TRUE)
+
+#takes only first 2000 with highest sd, will differ most in gene expression
 subdata <- test_data[ind[1:subnumber],]
 
 tsubdata <- data.frame(t(subdata[,-7]))
@@ -35,7 +37,9 @@ rownames(tsubdata) = c("first1", "first2", "first3", "second1", "second2", "seco
 numberclusters <- parameters[1]
 
 tsubdata_eucl_cor <- as.dist(1-cor(tsubdata))
+# 
 tsubdata_cluster_cor = hclust(tsubdata_eucl_cor, method=method)
+# split the data into 6 clusters
 tsubdata_tree_cluster_cor = cutree(tsubdata_cluster_cor, k=numberclusters)
 
 table(tsubdata_tree_cluster_cor)
@@ -49,6 +53,7 @@ draw_graph_cluster <- function(clustnumber, colornumber) {
   }
 }
 
+# still need to write the plots to a file
 par(mfrow = c(2, 3))
 for (i in 1:numberclusters) {
   draw_graph_cluster(i, i)
@@ -59,6 +64,11 @@ target <- tsubdata_tree_cluster_cor[g.o.i]
 names(target)<-'Cluster containing the gene of interest'
 cl.sel <- which(tsubdata_tree_cluster_cor == target)
 cl.sel      # this will show you all the genes in the gene cluster you selected
+cl.sel.df <- as.data.frame(cl.sel)
+
+# write all the interesting genes to a file
+write.table(cl.sel.df, file="Interesting_genes.csv", sep=",", row.names=TRUE)
+
 target      # this will show you which cluster you have extracted
 
 draw_graph_cluster(target,target)
